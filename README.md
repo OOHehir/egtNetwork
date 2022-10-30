651c079c0365acecc11e04cb8beba0117486c51f
leo389 <40183643+leo389@users.noreply.github.com>
2022-10-30 10:03:21 +0000

    Add screenshot

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # An EGT based GUI application that displays network stats
 
-Code is based on the [egt](http://github.com/linux4sam/egt) gui application & is purely C++ based. To build you'll need egt installed on your machines(s).The code build binaries for x86 & ARM devices. 
+A simple gui application based on [egt](http://github.com/linux4sam/egt) that is purely C++ based.
 
 ***Note: EGT is primarily targeted at Microchip SAM ARM devices***
 
-## To Build
+## To Buil
+You'll need egt installed on your machines(s).
+To cross-compile you'll need the Yocto/Poky SDK or another method
 To build both x86 & ARM binaries run the included build.sh script
 
 ***Note: The scrip invokes the [Yocto/Poky SDK env](http://docs.yoctoproject.org/sdk-manual/working-projects.html#makefile-based-projects), you'll need to modify to support yours***
+
+![Screenshot](/screenshot/image1.png)
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ NC='\033[0m' # No Color
 WARN_CFLAGS="-Wall -Wextra -pedantic -Wfloat-equal -Wsign-compare -Wpointer-arith -Wwrite-strings -Wmissing-declarations -Wpacked -Wstrict-aliasing=2 -Winit-self -Wno-attributes -Wno-long-long -Wno-missing-field-initializers -Wno-unused-parameter -Wno-psabi"
 
 # Script to build source code to executable
-if g++ -std=c++14 egtNetwork.cpp -o egtNetwork_x86 -O $WARN_CFLAGS `pkg-config libegt --cflags --libs` -lgpiod -pthread -lsqlite3; then
+if g++ -std=c++14 egtNetwork.cpp -o egtNetwork_x86 -O $WARN_CFLAGS `pkg-config libegt --cflags --libs` -pthread ; then
     echo "x86 build success"
 else
     echo -e "x86 build ${RED}FAILURE${NC}, exiting"
@@ -18,7 +18,7 @@ fi
 . /opt/poky-atmel/3.1.15/environment-setup-cortexa5t2hf-neon-vfpv4-poky-linux-gnueabi
 
 # Build for ARM
-if $CXX $INC_DIR egtNetwork.cpp -o egtNetwork_ARM -O $WARN_CFLAGS `pkg-config libegt --cflags --libs` -lgpiod -pthread -lsqlite3; then
+if $CXX $INC_DIR egtNetwork.cpp -o egtNetwork_ARM -O $WARN_CFLAGS `pkg-config libegt --cflags --libs` -pthread; then
     echo "ARM build success"
     # use -r to recursively copy files
     # Uncomment below & update to copy to board
